5cd23fed28b785aa702b652212cbf530bd07ac57
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2022-08-18 20:18:50 +0200

    code cleanup

--- KeysRegister/Forms/SelectEmployeeForm.cs
+++ KeysRegister/Forms/SelectEmployeeForm.cs
@@ -1,15 +1,5 @@
-﻿using KeysRegister.Data;
-using KeysRegister.Entities;
+﻿using KeysRegister.Entities;
 using KeysRegister.Services;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace KeysRegister.Forms
 {
